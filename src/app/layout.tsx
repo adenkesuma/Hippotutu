48cbc2e55cef2a3c8e468d5643bd9b44d3640eaf
adenkesuma <adenfdfd10@gmail.com>
2023-12-02 10:24:55 +0700

    add logo icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google'
 
 import './globals.css'
 import { cn } from '@/lib/utils'
+import Navbar from '@/components/Navbar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn('relative h-full font-sans antialiased', inter.className)}>
         <main className="relative flex flex-col min-h-screen">
+          <Navbar />
           {children}
         </main>
       </body>
