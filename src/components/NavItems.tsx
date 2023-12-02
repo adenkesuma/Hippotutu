cc5cdcdda81de860c19a937eb7e2cad1cfa604dd
adenkesuma <adenfdfd10@gmail.com>
2023-12-02 11:19:35 +0700

    `feat:` add useOnClickOutsite

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,19 @@
 "use client"
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 import { PRODUCT_CATEGORIES } from "@/config"
+import { useOnClickOutsite } from "@/hooks/use-on-click-outsite"
 import NavItem from "./NavItem"
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null)
 
   const isAnyOpen = activeIndex !== null
+
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  useOnClickOutsite(navRef, () => setActiveIndex(null))
 
   return (
     <div className="flex gap-4 h-full">
