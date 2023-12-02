d492d3b3fdcb5075ab849aae6819ae791033893c
adenkesuma <adenfdfd10@gmail.com>
2023-12-02 11:25:42 +0700

    `feat:` add escape function keydown

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,27 @@
-"use client"
+'use client'
 
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from 'react'
 
-import { PRODUCT_CATEGORIES } from "@/config"
-import { useOnClickOutsite } from "@/hooks/use-on-click-outsite"
-import NavItem from "./NavItem"
+import { PRODUCT_CATEGORIES } from '@/config'
+import { useOnClickOutsite } from '@/hooks/use-on-click-outsite'
+import NavItem from './NavItem'
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+
+    document.addEventListener('keydown', handler)
+
+    return () => {
+      document.removeEventListener('keydown', handler)
+    }
+  }, [])
 
   const isAnyOpen = activeIndex !== null
 
@@ -16,7 +30,7 @@ const NavItems = () => {
   useOnClickOutsite(navRef, () => setActiveIndex(null))
 
   return (
-    <div className="flex gap-4 h-full">
+    <div className='flex gap-4 h-full' ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const handleOpen = () => activeIndex === index ? setActiveIndex(null) : setActiveIndex(index)
 
