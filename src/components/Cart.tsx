fb0ac8ca9cce3d59574d57cf14ce91e780d79648
adenkesuma <adenfdfd10@gmail.com>
2023-12-05 10:32:03 +0700

    `feat:` add cart component by shadcn

---
+++ src/components/Cart.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import { ShoppingCart } from 'lucide-react'
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
+import { Separator } from './ui/separator'
+
+const Cart = () => {
+  const itemCount = 1
+
+  return (
+    <Sheet>
+      <SheetTrigger className='group -m-2 flex items-center p-2'>
+        <ShoppingCart className='h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500' aria-hidden='true' />
+
+        <span className='ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800'>
+          0
+        </span>
+      </SheetTrigger>
+
+      <SheetContent className='flex w-full flex-col pr-0 sm:max-w-lg'>
+        <SheetHeader className='space-y-2.5 pr-6'>
+          <SheetTitle>
+            Cart (0)
+          </SheetTitle>
+        </SheetHeader>
+
+        {itemCount > 0 ? (
+          <>
+            <div className="flex w-full flex-col pr-6">
+              {/* cart logic */}
+              cart items
+            </div>
+            <div className="space-y-4 pr-6">
+              <Separator />
+              <div className="space-y-1.5 text-sm">
+                <div className="flex">
+                  <span className="flex-1">Shipping</span>
+                  <span>Free</span>
+                </div>
+                <div className="flex">
+                  <span className="flex-1">Transaction Fee</span>
+                  <span>1</span>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div></div>
+        )}
+      </SheetContent>
+    </Sheet>
+  )
+}
+
+export default Cart
+
+
