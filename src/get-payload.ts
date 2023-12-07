87bdf3a96468ab989bdfe7754e70e4afd02859d3
adenkesuma <adenfdfd10@gmail.com>
2023-12-07 13:57:50 +0700

    `fix:` fix typo problem exec

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -20,9 +20,9 @@ interface Args {
   initOptions?: Partial<InitOptions>
 }
 
-export const getPayloadClient = async ({ initOptions } : Args) => {
+export const getPayloadClient = async ({ initOptions } : Args = {}) => {
   if (!process.env.PAYLOAD_SECRET) {
-    throw new Error('Missing PAYLOAD_SECRET environment variable')
+    throw new Error('Missing PAYLOAD_SECRET')
   }
 
   if (cached.client) {
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,11 +3,6 @@ import { slateEditor } from '@payloadcms/richtext-slate'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import path from 'path'
-import dotenv from 'dotenv'
-
-dotenv.config({
-  path: path.resolve(__dirname, '../.env')
-})
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
