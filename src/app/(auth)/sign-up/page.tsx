451e2bbbdf07a89b45d755803396ae112598d71f
adenkesuma <adenfdfd10@gmail.com>
2023-12-07 15:05:46 +0700

    `feat:` add sign-up form

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+
+import { Button, buttonVariants } from '@/components/ui/button'
+import { Icons } from '@/components/Icons'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { cn } from '@/lib/utils'
+
+const Page = () => {
+  return (
+    <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
+      <div className='mx-auto w-full flex flex-col justify-center space-y-6 sm:w-96'>
+        <div className='flex flex-col items-center space-y-2 text-center'>
+          <Icons.Logo className='h-20 w-20' />
+          <h1 className='text-2xl font-bold'>
+            Create an account
+          </h1>
+          <Link href='/sign-in' className={buttonVariants({ variant: 'link', className: 'gap-1.5' })}>
+            Already have an account ? Sign-in
+            <ArrowRight className='h-4 w-4' />
+          </Link>
+        </div>
+
+        <div className="grid gap-6">
+          <form onSubmit={() => {}}>
+            <div className="grid gap-2">
+              <div className="grid gap-1 py-2">
+                <Label htmlFor='email'>Email</Label>
+                <Input 
+                  className={cn({ 'focus-visible:ring-red-500': true })}
+                  placeholder='you@example.com'
+                />
+              </div>
+              <div className="grid gap-1 py-2">
+                <Label htmlFor='password'>Password</Label>
+                <Input 
+                  className={cn({ 'focus-visible:ring-red-500': true })}
+                  placeholder='password'
+                />
+              </div>
+              <Button className='mt-2'>Sign Up</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Page 
